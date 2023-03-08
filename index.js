d4dd9decc5663130abfc847e49b102be6d60987a
Afridi <afridi.dcc6251@gmail.com>
2023-03-08 18:58:27 +0600

    add

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('admission').addEventListener('click',function(){
+  window.location.href = 'course.html'
+})
+document.getElementById('home').addEventListener('click',function(){
+  window.location.href = 'index.html'
+})
